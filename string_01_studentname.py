8abf5b2c0f453dcd542bc883f509ba93ab39b0da
Jinho Seo <jinho.seo.165@outlook.com>
2018-10-16 03:52:19 -0700

    string_01_studentname
    Introduction of python - string(1), using index in a string.

---
+++ string_01_studentname.py
@@ -0,0 +1,9 @@
+# assign string to student_name
+student_name = "Alton"
+
+# the first element is always index = 0
+print(student_name[0], "<-- first character at index 0")
+print(student_name[1])
+print(student_name[2])
+print(student_name[3])
+print(student_name[4])
